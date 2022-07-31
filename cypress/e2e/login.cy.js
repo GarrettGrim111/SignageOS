248f6f5c8f50b657ebfbc91762ac7f7443218aa6
Garrett <garrett.grim111@gmail.com>
2022-07-31 12:12:39 +0200

    identificator of page load exchanged for more reliable element

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,9 +6,8 @@ describe('Test', () => {
 const email = "garrett.grim111@gmail.com"
 const pass = "Tester!123"
 
-    cy.visit('https://box.signageos.io/')
-
-    cy.writeFile('cypress/test_result.txt', 'visit https://box.signageos.io/\n', { flag: 'a+' })
+  cy.visit('https://box.signageos.io/')
+  cy.writeFile('cypress/test_result.txt', 'visit https://box.signageos.io/\n', { flag: 'a+' })
 
 
   // this could be used when we want to make assertion on ANY data inside input (at least one word - may include numbers but not special characters like @ or .)
@@ -30,8 +29,8 @@ const pass = "Tester!123"
   //   expect(value).to.match(new RegExp(email))
   // })
 
-  // this could be used when we want to make assertion on Integers inside input (only number can be inputed)
-  // this implementation results into failed test due to used password (contains not a number elements)
+  // this could be used when we want to make assertion on Integers inside input (only numbers can be inputed)
+  // this implementation results into failed test as expected due to used password (contains not a number elements)
 
   // cy.get('#password')
   // .type(pass)
@@ -50,16 +49,15 @@ const pass = "Tester!123"
 
   cy.writeFile('cypress/test_result.txt', 'Get password input, type inside value, check if value contains at least one number\n', { flag: 'a+' })
 
-    cy.get("[data-testid=submit]").click()
-    cy.writeFile('cypress/test_result.txt', 'Click on submit\n', { flag: 'a+' })
+  cy.get("[data-testid=submit]").click()
+  cy.writeFile('cypress/test_result.txt', 'Click on submit\n', { flag: 'a+' })
 
 
-    cy.get('[data-testid=modal-header]', { timeout: 30000 })
-    cy.writeFile('cypress/test_result.txt', 'Wait for the page to load\n', { flag: 'a+' })
+  cy.get('[data-testid=page-header]', { timeout: 30000 })
+  cy.writeFile('cypress/test_result.txt', 'Wait for the page to load\n', { flag: 'a+' })
 
-    cy.url().should('include', 'box')
-    cy.writeFile('cypress/test_result.txt', 'Make sure URL contains word "box"\n', { flag: 'a+' })
-
-
+  cy.url().should('include', 'box')
+  cy.writeFile('cypress/test_result.txt', 'Make sure URL contains word "box"\n', { flag: 'a+' })
+  cy.writeFile('cypress/test_result.txt', 'Test finished all steps\n', { flag: 'a+' })
   })
 })
